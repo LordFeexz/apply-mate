c8b7a6710c98cdd66a4a112e73fe4dec325bbf9b
anandafiqri <anandafiqri1403@gmail.com>
2025-04-10 12:57:08 +0700

    fix: nested html

--- src/app/layout.tsx
+++
@@ -1,7 +0,0 @@
-import RootLayout from "@/components/layouts/root";
-import { LANG } from "@/enums/global";
-import type { ChildrenProps } from "@/interfaces/component";
-
-export default function BaseLayout({ children }: ChildrenProps) {
-  return <RootLayout lang={LANG.EN}>{children}</RootLayout>;
-}
